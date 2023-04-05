e15becb866337aca283f8b16c8d890be6c02e246
Tania Sasaran <taniasasaran@gmail.com>
2023-04-05 19:14:14 +0300

    created a skeleton for frontend

--- backend/src/main/java/com/example/lab1jv/AppLibrary.java
+++ backend/src/main/java/com/example/lab1jv/AppLibrary.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@Component
 public class AppLibrary {
 	public static void main(String[] args) {
 		SpringApplication.run(AppLibrary.class, args);
